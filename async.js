67d912188df36f475f65c4b239a455363aad3703
SomratIIT <101197949+SomratIIT@users.noreply.github.com>
2024-12-10 10:00:00 +0600

    js=>single thread & synchronous

--- async.js
+++ async.js
@@ -1,1 +1,10 @@
- 
+//  console.log(1)
+//  console.log(2)
+// setTimeout(doSomething,3000)
+//  console.log(4)
+//  console.log(5)
+//  console.log(6)
+//  function doSomething()
+//  {
+//     console.log(3);
+//  }
---
+++ sync.js
@@ -0,0 +1,7 @@
+console.log('I');
+
+setTimeout(()=>{
+    console.log('Ice Cream')
+},3000)
+
+console.log('Eat');
